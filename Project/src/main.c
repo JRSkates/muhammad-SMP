7c09c8888bb85b162f03bf36c1529acd54125412
JRSkates <skatesproduction@gmail.com>
2025-01-24 14:58:33 +0000

    Project cleanup

--- Project/src/main.c
+++ Project/src/main.c
@@ -32,6 +32,7 @@ int main(void) {
         scanf("%d", &user_choice);
         switch (user_choice) {
             case 1: { 
+                // Add Student
                 char *student_name = malloc(50);
 
                 if (!student_name) {
@@ -256,34 +257,6 @@ int main(void) {
 
         }
     }
-    // Add subjects
-    // char *subj_name = "Maths";
-    // char *teacher_fullname = "John";
-    // add_subject(&subject_head, &subj_name, &teacher_fullname);
-
-    // char *subj_name2 = "English";
-    // char *teacher_fullname2 = "Jane";
-    // add_subject(&subject_head, &subj_name2, &teacher_fullname2);
-
-    // // Display all subjects
-    // printf("Subjects:\n");
-    // display_all_subjects(&subject_head);
-
-    // // Find a subject
-    // char *search_subject = "Maths";
-    // struct Subject *found = find_subject(&subject_head, &search_subject);
-    // if (found != NULL) {
-    //     printf("Found Subject: %s, Teacher: %s\n", found->subject_name, found->teacher_fullname);
-    // } else {
-    //     printf("Subject not found\n");
-    // }
-
-    // // Add students
-    // char *stu_name = "John Doe";
-    // char *grade = "A";
-    // add_student(&student_head, &stu_name, &subj_name, &grade);
-
-    // // Future: Display students
 
     return 0;
 }
